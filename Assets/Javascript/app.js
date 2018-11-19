051fd7db0625e7194a50198e79a16a1760a598fc
Just Nick <nicholas.babington@hotmail.com>
2018-11-19 16:36:45 -0500

    Still need timer stuff

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -16,23 +16,44 @@
   fMinutesAway = 0;
   var nextArrival = new Date();
 
+  database.ref('/train').on("child_added",function(snapshot){
+    var trainName = snapshot.val().trainName;
+   var destination = snapshot.val().destination;
+   var frequency = snapshot.val().frequency;
+   var firstTrain = snapshot.val().firstTrain;
+    var currentTime = 
+    console.log(trainName)
+    
+    
+   
+  
+    
+  
+        // Add each train's data into the table
+   $("#trainTable").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + firstTrain + "</td><td>" + "15 minutes" + "</td></tr>");
+   
+
+
+  });
   $("#go").on("click", function(event) {
     event.preventDefault();
     var fTrainName = $("#train-name").val().trim();
     var fDestination = $("#destination").val().trim();
     var fFirstTrainTime = $("#first-train-time").val().trim();
     var fFrequency = $("#frequency").val().trim();
-    console.log("Employee Name "+fTrainName);
-    console.log("Role "+fDestination);
-    console.log("Start Date "+fFirstTrainTime);
-    console.log("Monthly Rate "+fFrequency);
+    $('#train-name').empty();
+    $('#destination').empty();
+    $('#first-train-time').empty();
+    $('#frequency').empty();
+   
 
     database.ref('/train').push({
       trainName: fTrainName,
       destination: fDestination,
       firstTrainTime: fFirstTrainTime,
       frequency: fFrequency,
-      
+     
+
     });
   
 
